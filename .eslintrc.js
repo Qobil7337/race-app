942a949375257c7928f185dea6d916bca2496692
Qobil7337 <qobil7337@mail.ru>
2024-04-01 16:57:42 +0500

    fix: set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any additional rules or overrides here
+  },
+};
