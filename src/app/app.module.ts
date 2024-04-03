de93bf58ec0b944eb1973f14ac3b0d8b4f5cfa63
Qobil7337 <qobil7337@mail.ru>
2024-04-03 08:50:39 +0500

    fix: install ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {provideHttpClient} from "@angular/common/http";
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {NzPaginationComponent} from "ng-zorro-antd/pagination";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideHttpClient()],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, NzPaginationComponent],
+  providers: [provideHttpClient(), { provide: NZ_I18N, useValue: en_US }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
